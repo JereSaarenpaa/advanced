aa0bc37b8a769aff8d2b7b6fec47d7fcb75c4bef
Mikko <mikko.luhtasaari@tuni.fi>
2020-01-17 13:48:06 +0200

    Add prints

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,16 @@
 print("Hello world!")
+<<<<<<< HEAD
+=======
+print("Not another hello world")
+print("No")
+<<<<<<< HEAD
+=======
+print("More")
+print("Hello")
+print("World")
+<<<<<<< HEAD
+print("Please, I'm begging you")
+=======
+>>>>>>> c3cf8d5 (qwerty)
+>>>>>>> 9d5dc3a (Add prints)
+>>>>>>> 5e91b42 (Add prints)
